cdd1be0c90984332ef61c1d47b0e5f525c60d1cd
Nhat01 <nguyennhatnguyen2@gmail.com>
2022-12-18 20:48:08 +0700

    lan1

---
+++ index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv'
+import cors from "cors";
+import authRoute from './routes/auth.js'
+import userRoute from './routes/user.js'
+import productRoute from './routes/product.js'
+import cartRoute from './routes/cart.js'
+import orderRoute from './routes/order.js'
+const app = express();
+
+mongoose.set('strictQuery', false);
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB Connection Successfull!"))
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.use(cors());
+app.use(express.json());
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
+// app.use("/api/checkout", stripeRoute);
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Backend server is running!");
+});
